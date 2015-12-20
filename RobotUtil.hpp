7f4b94823d971dc821d1c91af33bd429832159fc
Owl <owl8hukurou@gmail.com>
2015-12-20 15:45:43 +0000

    ScrpMasterの動作確認をした

--- RobotUtil.hpp
+++ RobotUtil.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "scrpSlave.hpp"
-#include "scrpMaster.hpp"
-#include "motor.hpp"
-#include "ds3Read.hpp"
+#include "ScrpSlave.hpp"
+#include "ScrpMaster.hpp"
+#include "Motor.hpp"
+#include "Ds3Read.hpp"
