1deffbfbbcc286b22b7c0944c67500617f977599
Owl8 <owl8hukurou@gmail.com>
2015-12-21 11:28:55 +0900

    Motorを実装

--- src/Motor.hpp
+++ src/Motor.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "ScrpMaster.hpp"
+
+namespace rbutil {
+  extern int MaxMotorPower;
+  class Motor {
+  public :
+    Motor();
+    Motor(unsigned char, unsigned char, double, ScrpMaster&, short maxPower = MaxMotorPower);
+    Motor(MotorDataFormat, ScrpMaster&, short maxPower = MaxMotorPower);
+    short changeMaxPower(short);
+    virtual bool spin(short, bool asyncFlag = false);
+    virtual ~Motor();
+  protected :
+    ScrpMaster* sm;
+    unsigned char id;
+    unsigned char mNum;
+    double magni;
+    short maxPower;
+    bool initFlag;
+  };
+  int loadMotorSetting(char*, MotorDataFormat*, int);
+}
